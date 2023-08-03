d07f25a5bbd8dd2b4873a5543d25b226efd8a741
subhrajitdeb <subhrajitdeb54@gmail.com>
2023-08-04 01:59:18 +0530

    Remove duplicates from unsorted ll - 4/8/23

--- src/tulesko_ll/RemoveDuplicateEle.java
+++ src/tulesko_ll/RemoveDuplicateEle.java
@@ -6,18 +6,22 @@ public class RemoveDuplicateEle {
         Node f1 = new Node();
         f1.data = 1;
         Node f2 = new Node();
-        f2.data = 2;
+        f2.data = 3;
         Node f3 = new Node();
-        f3.data = 2;
+        f3.data = 1;
         Node f4 = new Node();
-        f4.data = 3;
+        f4.data = 1;
 
         f1.next = f2;
         f2.next = f3;
         f3.next = f4;
         f4.next = null;
 
-        removeDulicate(f1);
+        //remove duplicate from sorted ll
+        //removeSortedDulicate(f1);
+
+        //remove duplicate from un sorted ll
+        removeFromUnSortedLL(f1);
     }
 
     public void delete(Node node){
@@ -28,7 +32,7 @@ public class RemoveDuplicateEle {
         }
     }
 
-    public void removeDulicate(Node head){
+    public void removeSortedDulicate(Node head){
         Node curr = head;
         if(head == null){
             return;
@@ -43,6 +47,39 @@ public class RemoveDuplicateEle {
             }else {
                 curr = curr.next;
             }
+        }
+
+        printLinkedList(head);
+    }
+
+    //1 - 3 - 2 - 3 - null
+
+    public void removeFromUnSortedLL(Node head){
+        Node curr = head;
+        if(head == null){
+            return;
+        }
+
+        while (curr != null){
+            if(curr.next != null){
+                Node next = curr.next;
+                Node prev = curr;
+                while (next != null){
+                    if(curr.data == next.data){
+                        Node next_next = next.next;
+                        Node nodeToDelete = next;
+
+                        //delete
+                        prev.next = next_next;
+                        next = next.next;
+                        nodeToDelete.next = null;
+                    }else {
+                        next = next.next;
+                        prev = prev.next;
+                    }
+                }
+            }
+            curr = curr.next;
         }
 
         printLinkedList(head);
