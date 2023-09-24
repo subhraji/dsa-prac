de2e6b4a6fdadea0ed27dd7eacb2460c3901bbd5
subhrajitdeb <subhrajitdeb54@gmail.com>
2023-09-24 23:54:22 +0530

    find subarray of sum - 24/9/23

--- src/Gfg.java
+++ src/Gfg.java
@@ -62,6 +62,29 @@ public class Gfg {
         return result;
     }
 
+    static ArrayList<Integer> subarraySum3(int[] arr, int n, int s)
+    {
+        int sum = 0;
+        int i = 0;
+        ArrayList<Integer> newArray = new ArrayList<Integer>();
+
+        for(int j = 0; j<n; j++){
+            sum += arr[j];
+            while(sum>s){
+                sum -= arr[i];
+                i++;
+            }
+            if((sum == s) && (i<=j)){
+                newArray.add(i+1);
+                newArray.add(j+1);
+                return newArray;
+            }
+        }
+
+        newArray.add(-1);
+        return newArray;
+    }
+
     private static void printArray(int arr[]){
         for (int i = 0; i<=arr.length-1; i++){
             System.out.print(arr[i]);
