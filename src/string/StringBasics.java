853c9e5cd6e61abbd18837f8f55616d46a65c59a
subhrajitdeb <subhrajitdeb54@gmail.com>
2024-01-09 01:41:18 +0530

    dsa prac - 4/10/2024

--- src/string/StringBasics.java
+++ src/string/StringBasics.java
@@ -2,11 +2,10 @@ package string;
 
 public class StringBasics {
     public static void main(String[] args) {
-        String str = "Hello World";
-        /*char[] arr = str.toCharArray();
-        for (int i = 0; i<arr.length; i++) {
-            System.out.print(arr[i]);
-        }*/
-        System.out.println(str.indexOf(' '));
+        String str = "Hello";
+        System.out.println(str.substring(2));
+
+        //problem solve
+        //51 min
     }
 }
