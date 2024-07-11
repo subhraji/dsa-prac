9c3552c6f22648a106b576300da1372c7452a015
subhrajit <subhrajit.sumatoglobal@gmail.com>
2024-07-11 13:26:12 +0530

    advance pattern

--- src/pattern/AdvancePattern.java
+++ src/pattern/AdvancePattern.java
@@ -3,7 +3,7 @@ package pattern;
 public class AdvancePattern {
     public static void main(String[] args) {
         AdvancePattern advancePattern = new AdvancePattern();
-        advancePattern.diamondPattern();
+        advancePattern.diamondPattern2();
     }
 
     private void butterflyPattern(){
@@ -127,6 +127,44 @@ public class AdvancePattern {
             }
 
             for (int j = 2*n-i; j>=i; j--){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    private void diamondPattern2(){
+        int n = 5;
+
+        for (int i = 1; i<=n; i++){
+
+            for (int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            for (int j = 2; j<=i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 1; i<= n; i++){
+
+            for (int j = 2; j<=i; j++){
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <=n-i+1; k++){
+                System.out.print("*");
+            }
+
+            for (int k = 2; k <=n-i+1; k++){
                 System.out.print("*");
             }
 
